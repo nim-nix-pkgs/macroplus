6e329f452b0f58932e432d62ed7a785e0163da8e
hugosenari <hugosenari@gmail.com>
2022-03-23 22:39:23 -0300

    feat(nimpkgs): remove lock

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''a collection of constants and tiny utilities that focuses on readability'';
+  description = ''a collection of constants and small procs that focues on readability'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-macroplus-main.flake = false;
-  inputs.src-macroplus-main.ref   = "refs/heads/main";
-  inputs.src-macroplus-main.owner = "hamidb80";
-  inputs.src-macroplus-main.repo  = "macroplus";
-  inputs.src-macroplus-main.type  = "github";
+  inputs.src-macroplus-0_2_0.flake = false;
+  inputs.src-macroplus-0_2_0.ref   = "refs/tags/0.2.0";
+  inputs.src-macroplus-0_2_0.owner = "hamidb80";
+  inputs.src-macroplus-0_2_0.repo  = "macroplus";
+  inputs.src-macroplus-0_2_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-macroplus-main"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-macroplus-0_2_0"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-macroplus-main";
+    src  = deps."src-macroplus-0_2_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
